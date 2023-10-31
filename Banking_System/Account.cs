791df811a9de3c82ebfb3d5fed29857bdb2d8a30
andrew <andrew@DESKTOP-77O1GIB>
2023-10-31 09:31:01 +0000

    Add project files.

---
+++ Banking_System/Account.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_System
+{
+    public class Customer
+    {
+        public string Fullname { get; set; }
+
+        public List<Account> Accounts { get; } = new List<Account>();
+
+        public Customer(string fullname)
+        {
+            Fullname = fullname;
+        }
+
+        public void AddAccount(Account account)
+        {
+            Accounts.Add(account);
+        }
+
+        public void RemoveAccount(Account account)
+        {
+            Accounts.Remove(account);
+        }
+
+
+    }
+    public class Account
+    {
+        public int AccountID { get; }
+        public Customer Owner { get; set; }
+        public decimal Balance { get; set; }
+
+        private static int accountNumberSeed = 1;
+        public bool IsClosed { get; set; }
+
+        public Account(Customer owner, decimal initial_balance)
+        {
+            Owner = owner;
+            this.Balance = initial_balance;
+            this.AccountID = accountNumberSeed;
+            this.IsClosed = false;
+
+            accountNumberSeed++;
+            Console.WriteLine($"Welcome {Owner.Fullname} to your new account. Your new account ID is {this.AccountID}, and your current balance is £{this.Balance}.");
+        }
+
+        public void CloseAccount()
+        {
+            IsClosed = true;
+            Console.WriteLine($"Account {AccountID} has been closed.");
+        }
+
+        public void Deposit(int accountNumber, decimal amount)
+        {
+            if (this.AccountID == accountNumber)
+            {
+                this.Balance += amount;
+                Console.WriteLine($"You have successfully added £{amount} into your deposit.\nYour new balance is £{this.Balance}.");
+            }
+            else
+            {
+                Console.WriteLine("The account number provided does not match this account.");
+            }
+        }
+
+        public void Withdraw(int accountNumber, decimal amount)
+        {
+            if (this.AccountID == accountNumber && this.Balance >= amount)
+            {
+                this.Balance -= amount;
+                Console.WriteLine($"You have successfully withdrawn £{amount}.\nYour new balance is £{this.Balance}.");
+            }
+            else
+            {
+                Console.WriteLine("The account number provided does not match this account or insufficient funds.");
+            }
+        }
+
+        public void Transfer(Account toAccount, decimal amount)
+        {
+            if (!IsClosed && Balance >= amount)
+            {
+                Balance -= amount;
+                toAccount.Balance += amount;
+                Console.WriteLine($"£{amount} has been transferred to account {toAccount.AccountID}.");
+            }
+            else
+            {
+                Console.WriteLine("Transfer failed. Check account status or insufficient funds.");
+            }
+        }
+    }
+}
---
+++ Banking_System/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Banking_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bank Systems");
+            Console.Write("\nWould you like to open up a new account? Yes or No: ");
+
+            var response = Console.ReadLine();
+            if (response.ToLower() == "yes")
+            {
+                Console.Write("\nOk brilliant. As a new customer, can you provide your full name please: ");
+                Customer customer = new Customer(Console.ReadLine());
+                Console.Write("\nThank you, for your new bank account, can you enter your initial balance: £");
+                var deposit = decimal.Parse(Console.ReadLine());
+                Account account1 = new Account(customer, deposit);
+                customer.AddAccount(account1);
+                var current_account = customer.Accounts[0];
+
+                var InBank = true;
+                while (InBank)
+                {
+                    Console.Write("\nPlease select one of the options below:\n1 - Withdraw\n2 - Deposit\n3 - Transfer\n4 - New Account\n5 - Switch Account\n6 - View Balance\n7 - Exit\nOption: ");
+                    var option = Console.ReadLine();
+
+                    if (option == "1")
+                    {
+                        Console.Write("\nPlease enter the value you like to withdraw: £");
+                        var value = decimal.Parse(Console.ReadLine());
+                        current_account.Withdraw(current_account.AccountID, value);
+                    }
+                    else if (option == "2")
+                    {
+                        Console.Write("\nPlease enter the value you like to deposit: £");
+                        var value = decimal.Parse(Console.ReadLine());
+                        current_account.Deposit(current_account.AccountID, value);
+                    }
+                    else if (option == "3")
+                    {
+                        if (customer.Accounts.Count > 1)
+                        {
+                            for (int i = 0; i < customer.Accounts.Count; i++)
+                            {
+                                var accountId = $"{i + 1} - {customer.Accounts[i].AccountID}";
+                                if (current_account.AccountID == customer.Accounts[i].AccountID)
+                                {
+                                    accountId += " - [Your current account]";
+                                }
+                                Console.WriteLine(accountId);
+                            }
+                            Console.Write("\nPlease select one of the accounts that you like to transfer? Use the index number: ");
+                            var account_option = int.Parse(Console.ReadLine()) - 1;
+                            var to_account = customer.Accounts[account_option];
+                            if (current_account != to_account)
+                            {
+                                Console.Write("\nHow much will you like to transfer: £");
+                                var transfer = decimal.Parse(Console.ReadLine());
+                                current_account.Transfer(to_account, transfer);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry you need another account to do the transfer.\n");
+                        }
+                    }
+                    else if (option == "4") 
+                    {
+                        Console.Write("\nHow much initial balance would you like in your new account: ");
+                        var init_balance = decimal.Parse(Console.ReadLine());
+                        var new_account = new Account(customer, init_balance);
+                        customer.AddAccount(new_account);
+                    }
+                    else if (option == "5")
+                    {
+                        if (customer.Accounts.Count > 1)
+                        {
+                            for (int i = 0; i < customer.Accounts.Count; i++)
+                            {
+                                var accountId = $"Account {customer.Accounts[i].AccountID}, Balance: £{customer.Accounts[i].Balance}";
+                                if (current_account.AccountID == customer.Accounts[i].AccountID)
+                                {
+                                    accountId += " - [Your current account]";
+                                }
+                                Console.WriteLine(accountId);
+                            }
+                            Console.Write("Please select one of the accounts that you like to switch to? Use the index number: ");
+                            var account_option = int.Parse(Console.ReadLine()) - 1;
+                            current_account = customer.Accounts[account_option];
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry you only have one account.");
+                        }
+                    }
+                    else if (option == "6") 
+                    {
+                        Console.WriteLine($"Your current balance for this account is £{current_account.Balance}.");
+                    }
+                    else
+                    {
+                        InBank = false;
+                    }
+                }
+            }
+        }
+    }
+}
